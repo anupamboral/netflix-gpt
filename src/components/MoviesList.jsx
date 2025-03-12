6c5121541811d8173b8fc081ea7455db5dff5ac4
anupamboral <anupamboral6889@gmail.com>
2025-03-13 05:18:11 +0530

    removed some unnecessary code

--- src/components/MoviesList.jsx
+++ src/components/MoviesList.jsx
@@ -1,29 +1,6 @@
-import { useRef } from "react";
 import MovieCard from "./MovieCard";
-import { div } from "motion/react-client";
 
 const MoviesList = ({ title, movies }) => {
-  const scrollRef = useRef(null);
-
-  const prev = () => {
-    requestAnimationFrame(() => {
-      const scrollLeft = scrollRef.current.scrollLeft;
-      const itemWidth = parseInt(
-        getComputedStyle(scrollRef.current.children[0]).width
-      );
-      scrollRef.current.scrollLeft = scrollLeft - itemWidth * 1;
-    });
-  };
-
-  const next = () => {
-    requestAnimationFrame(() => {
-      const scrollLeft = scrollRef.current.scrollLeft;
-      const itemWidth = parseInt(
-        getComputedStyle(scrollRef.current.children[0]).width
-      );
-      scrollRef.current.scrollLeft = scrollLeft + itemWidth * 1;
-    });
-  };
   console.log(title, movies);
   return (
     <div className=" mb-10">
